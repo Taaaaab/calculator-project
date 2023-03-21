542850cbdb94ded7d7af6674f21995b97ff3b14d
Taaaaab <owen.whitsett@gmail.com>
2023-03-21 12:17:48 -0400

    Added types to buttons

--- src/script.ts
+++ src/script.ts
@@ -1,26 +1,30 @@
-const screenDisplay = document.querySelector(".screen") as HTMLDivElement;
-const buttons = document.querySelectorAll("button");
+const screenDisplay = document.querySelector(".screen") as Element | null;
+const buttons = document.querySelectorAll(
+  "button"
+) as NodeListOf<HTMLButtonElement>;
 
 let calculation: any[] = [];
 let accumulativeCalculation: string;
 
 function calculate(button: HTMLButtonElement) {
-  const value = button.textContent;
-  if (value === "CLEAR") {
-    calculation = [];
-    screenDisplay.textContent = ".";
-  } else if (value === "=") {
-    screenDisplay.textContent = eval(accumulativeCalculation);
-  } else {
-    calculation.push(value);
-    accumulativeCalculation = calculation.join("");
-    screenDisplay.textContent = accumulativeCalculation;
+  if (screenDisplay?.textContent) {
+    const value: string | null = button.textContent;
+    if (value === "CLEAR") {
+      calculation = [];
+      screenDisplay.textContent = ".";
+    } else if (value === "=") {
+      screenDisplay.textContent = eval(accumulativeCalculation);
+    } else {
+      calculation.push(value);
+      accumulativeCalculation = calculation.join("");
+      screenDisplay.textContent = accumulativeCalculation;
 
-    console.log(calculation);
+      console.log(calculation);
+    }
   }
 }
 
-buttons.forEach((button) =>
+buttons.forEach((button: HTMLButtonElement) =>
   button.addEventListener("click", () => calculate(button))
 );
 
